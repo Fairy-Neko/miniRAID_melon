b9ae8be8d5a528ccd0c815e4fda95e8c4d3998b7
Betairya <betairya@gmail.com>
2018-07-28 00:01:29 +0900

    make resources.js visible to git.

---
+++ build/js/resources.js
@@ -0,0 +1,57 @@
+game.resources = [
+    {
+        "name": "Cliff_tileset",
+        "type": "image",
+        "src": "data/img/Cliff_tileset.png"
+    },
+    {
+        "name": "crystalcoin",
+        "type": "image",
+        "src": "data/img/crystalcoin.png"
+    },
+    {
+        "name": "goldcoin",
+        "type": "image",
+        "src": "data/img/goldcoin.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "data/img/gripe_run_right.png"
+    },
+    {
+        "name": "ground_tiles",
+        "type": "image",
+        "src": "data/img/ground_tiles.png"
+    },
+    {
+        "name": "magical_girl",
+        "type": "image",
+        "src": "data/img/magical_girl.png"
+    },
+    {
+        "name": "object- layer",
+        "type": "image",
+        "src": "data/img/object- layer.png"
+    },
+    {
+        "name": "FixedSys",
+        "type": "image",
+        "src": "data/fnt/FixedSys.png"
+    },
+    {
+        "name": "playground",
+        "type": "tmx",
+        "src": "data/map/playground.tmx"
+    },
+    {
+        "name": "testmap",
+        "type": "tmx",
+        "src": "data/map/testmap.tmx"
+    },
+    {
+        "name": "FixedSys",
+        "type": "binary",
+        "src": "data/fnt/FixedSys.fnt"
+    }
+];
